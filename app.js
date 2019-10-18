ae20e120a62a661405c8ee6d9ed5f07fc13452e6
fenixqwe <53240682+fenixqwe@users.noreply.github.com>
2019-10-18 16:21:55 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,52 @@
+//Создать объект, который описывает ширину и высоту прямоугольника, а также может посчитать площадь фигуры
+
+/*const rectangle = {
+	width: 5,
+	height: 6,
+	getSquare () {
+		console.log(this.width * this.height)
+	}
+}
+rectangle.getSquare()*/
+//Создать объект, у которого будет цена товара и его скидка, а также два метода: для получения цены и для расчета цены с учетом скидки
+
+/*const price = {
+	price: 10,
+	discount: '15%',
+	getPriceWithDiscount () {
+		let disc = parseInt(this.discount) / 100 * this.price;
+		let sum = this.price - disc ;
+		console.log(sum)
+
+	}
+}
+price.getPriceWithDiscount()*/
+
+/*const height = {
+	height: 10,
+	increase () {
+		return this.height += 1
+	}
+}
+console.log(height.increase())*/
+//оздать объект “вычислитель”, у которого есть числовое свойство
+//“значение” и методы “удвоить”, “прибавить один”, “отнять один”.
+//Методы можно вызывать через точку, образуя цепочку методов
+
+
+/*const numerator = {
+	value: 3,
+	double () {
+		console.log(this.value *= 2)
+	},
+	plusOne () {
+		console.log(this.value += 1)
+	},
+	minusOne () {
+		console.log(this.value -= 1)
+	}
+
+}
+numerator.double()
+numerator.plusOne()
+numerator.minusOne()*/
